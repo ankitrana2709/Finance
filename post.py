e84e960b70091ba78bd6988c5324bd1138af5ce0
ankit rana <105877301+ankitrana2709@users.noreply.github.com>
2023-01-15 23:11:26 +0530

    ok

--- post.py
+++ post.py
@@ -1,6 +1,3 @@
-import sqlite3
-from sqlalchemy import create_engine
-from sqlalchemy.orm import scoped_session, sessionmaker
 """
 engine = create_engine(os.getenv("postgres://mxdbtvvr:aZWZQkZjukNsrrF2W85-kCV_-sLFIUgx@hansken.db.elephantsql.com/mxdbtvvr")) # database engine object from SQLAlchemy that manages connections to the database
                                                     # DATABASE_URL is an environment variable that indicates where the database lives
